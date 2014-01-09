d9a96401c8a34833787c4aabeb058aed85d956e5
Bruno Sato <bruno.sato@live.com>
2014-01-09 07:44:08 -0200

    fix

---
+++ jsfundamental/Lesson06/script.js
@@ -0,0 +1,10 @@
+var obj = "This is a string object.",
+    length = obj.length; // 24
+    
+var upperCase = obj.toUpperCase(),
+    lowerCase = obj.toLowerCase();
+
+
+alert(upperCase);
+alert(lowerCase);
+alert(obj);
