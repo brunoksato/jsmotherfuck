ecac526f172e0caad507d34a47f5bd831f372f6a
Bruno Sato <bruno.sato@live.com>
2014-01-10 00:17:14 -0200

    type of

---
+++ jsfundamental/bonus/script.js
@@ -0,0 +1,15 @@
+// Vamos criar algumas variáveis ou Operandos
+var teste1 = null;
+var teste2 = [1, 2, 3, 4, 5, 6];
+var teste3 = new Number(10);
+var teste4 = 10;
+var teste5 = new Array(1, 2, 3, 4, 5);
+var teste6 = "TutsMais";
+ 
+// Vamos Efetuar os testes com o console.log()
+alert(typeof teste1); // Olha a Bruxaria! O null é um objeto?!
+alert(typeof teste2); // Como é? O certo seria retornar um Array..
+alert(typeof teste3); // OK 10 é um número..
+alert(typeof teste4); // Correto sem bruxaria...
+alert(typeof teste5); // Não seria um Array? 
+alert(typeof teste6); // Ok é uma string 
