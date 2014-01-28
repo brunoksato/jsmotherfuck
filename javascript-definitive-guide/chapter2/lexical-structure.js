42067938daf6b701f31287c4efca4ba5d80657da
Bruno Sato <bruno.sato@live.com>
2014-01-28 15:28:16 -0200

    chapter2

---
+++ javascript-definitive-guide/chapter2/lexical-structure.js
@@ -0,0 +1,13 @@
+12 // The number twelve
+1.2 // The number one point two
+"hello world" // A string of text
+'Hi' // Another string
+true // A Boolean value
+false // The other Boolean value
+/javascript/gi // A "regular expression" literal (for pattern matching)
+null // Absence of an object
+{ x:1, y:2 } // An object initializer
+[1,2,3,4,5] // An array initializer
+
+var x = 0 // Semicolon omitted here
+;[x,x+1,x+2].forEach(console.log) // Defensive ; keeps this statement separate
